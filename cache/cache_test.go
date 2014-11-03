47128b38405472e9b868d774aae0fc3ae6910ffc
Oscar Campos <oscar.campos@member.fsf.org>
2014-11-03 16:02:46 +0000

    added multiplatform test for CacheDirectory

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -29,10 +29,36 @@ var _ = Describe("Cache", func() {
 			}
 		})
 
-		It("Should be true", func() {
+		It("Should Match", func() {
 			Expect(re.MatchString(cache.ExpandUser("~/VenGO"))).To(BeTrue())
 		})
+	})
 
+	Describe("CacheDirectory is a valid directory on each platform", func() {
+		var re *regexp.Regexp
+		Context("BSD and GNU/Linux", func() {
+			if runtime.GOOS != "darwin" && runtime.GOOS != "windows" {
+				re = regexp.MustCompile("/home/([a-z0-9]+)/.cache/VenGO")
+			}
+		})
+
+		Context("OS X", func() {
+			if runtime.GOOS == "darwin" {
+				re = regexp.MustCompile(
+					"/Users/([a-zA-Z0-9]+)/Library/Caches/VenGO")
+			}
+		})
+
+		Context("Windows", func() {
+			if runtime.GOOS == "windows" {
+				re = regexp.MustCompile(
+					"\\Users\\([a-zA-Z0-9]+)\\AppData\\VenGO")
+			}
+		})
+
+		It("Should Match", func() {
+			Expect(re.MatchString(cache.CacheDirectory())).To(BeTrue())
+		})
 	})
 
 })
