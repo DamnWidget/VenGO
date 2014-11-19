ecd1e67552ce495712b737c9ffcd116755a59fc4
Oscar Campos <oscar.campos@member.fsf.org>
2014-11-19 00:48:43 +0000

    command List finished

--- commands/commands_test.go
+++ commands/commands_test.go
@@ -1,6 +1,9 @@
 package commands_test
 
 import (
+	"encoding/json"
+	"strings"
+
 	"github.com/DamnWidget/VenGO/commands"
 )
 
@@ -12,8 +15,55 @@ var _ = Describe("Commands", func() {
 				l.ShowBoth = true
 			}
 			l := commands.NewList(showBoth)
+
 			Expect(l).ToNot(BeNil())
+			Expect(l.ShowBoth).To(BeTrue())
 		})
 	})
 
+	Describe("List", func() {
+		Describe("Run", func() {
+			Context("With an empty installation directory", func() {
+				Context("Asking for Both lists", func() {
+					var showBoth func(*commands.List)
+
+					BeforeEach(func() {
+						showBoth = func(l *commands.List) {
+							l.ShowBoth = true
+						}
+					})
+
+					It("Should return an empty installed and full non installed lists in Text format", func() {
+						l := commands.NewList(showBoth)
+
+						Expect(l).ToNot(BeNil())
+						versions, err := l.Run()
+
+						Expect(err).ToNot(HaveOccurred())
+						splitVers := strings.Split(versions, "\n")
+						Expect(splitVers[0]).To(Equal("Installed"))
+						Expect(splitVers[1]).To(Equal(""))
+						Expect(splitVers[2]).To(Equal("Available for Installation"))
+					})
+
+					It("Should return an empty installed and full non installed lists in Json format", func() {
+						jsonFormat := func(l *commands.List) {
+							l.DisplayAs = commands.Json
+						}
+						l := commands.NewList(showBoth, jsonFormat)
+
+						Expect(l).ToNot(BeNil())
+						versions, err := l.Run()
+
+						Expect(err).ToNot(HaveOccurred())
+						jsonVers := new(commands.BriefJSON)
+
+						Expect(json.Unmarshal([]byte(versions), jsonVers)).To(Succeed())
+						Expect(jsonVers.Installed).To(Equal([]string{}))
+						Ω(len(jsonVers.Available)).Should(BeNumerically(">", 100))
+					})
+				})
+			})
+		})
+	})
 })
--- commands/list.go
+++ commands/list.go
@@ -21,24 +21,34 @@
 package commands
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
 	"strings"
 
 	"github.com/DamnWidget/VenGO/cache"
+	"github.com/DamnWidget/VenGO/logger"
 )
 
 const (
-	Brief = iota
-	Extended
-	JsonBrief
-	JsonExtended
+	Text = iota
+	Json
 )
+
+// json brief output structure
+type BriefJSON struct {
+	Installed []string `json:"installed"`
+	Available []string `json:"available"`
+}
 
 // list command
 type List struct {
 	ShowInstalled    bool
 	ShowNotInstalled bool
 	ShowBoth         bool
-	Details          int
+	DisplayAs        int
 }
 
 // Create a new list and return back it's address
@@ -58,5 +68,132 @@ func NewList(options ...func(*List)) *List {
 // on the list options
 func (l *List) Run() (string, error) {
 	tags := cache.Tags()
-	return strings.Join(tags, "\n"), nil
+	sources := cache.AvailableSources()
+	binaries := cache.AvailableBinaries()
+
+	versions := []string{"Installed"}
+	installed, err := l.getInstalled(tags, sources, binaries)
+	if err != nil {
+		logger.Println("while running List command:", err)
+		return "error while running the command", err
+	}
+	if l.ShowBoth {
+		versions = append(versions, installed...)
+		versions = append(versions, "\nAvailable for Installation")
+		versions = append(
+			versions, l.getNonInstalled(installed, tags, sources, binaries)...)
+	} else {
+		if l.ShowInstalled {
+			versions = append(versions, installed...)
+		}
+		if l.ShowNotInstalled {
+			versions = append(versions, "\nAvailable for Installation")
+			versions = append(
+				versions, l.getNonInstalled(installed, tags, sources, binaries)...)
+		}
+	}
+
+	return l.display(versions)
+}
+
+// generates the output for the list command
+func (l *List) display(versions []string) (string, error) {
+	if l.DisplayAs == Text {
+		return strings.Join(versions, "\n"), nil
+	}
+
+	if l.DisplayAs == Json {
+		jsonData := &BriefJSON{[]string{}, []string{}}
+		doneInstalled := false
+		for _, v := range versions {
+			if v != "Installed" {
+				if !doneInstalled {
+					if v == "\nAvailable for Installation" {
+						doneInstalled = true
+						continue
+					}
+					v := strings.TrimLeft(v, "    ")
+					jsonData.Installed = append(jsonData.Installed, v)
+				} else {
+					v := strings.TrimLeft(v, "    ")
+					jsonData.Available = append(jsonData.Available, v)
+				}
+			}
+
+		}
+		data, err := json.Marshal(jsonData)
+		return string(data), err
+	}
+
+	return "", fmt.Errorf("List.DisplayAs is not set to a valid value!")
+}
+
+// return a list of installed go versions
+func (l *List) getInstalled(tags, sources, binaries []string) ([]string, error) {
+	files, err := filepath.Glob(filepath.Join(cache.CacheDirectory(), "*"))
+	if err != nil {
+		logger.Println("while getting installed versions:", err)
+		return nil, err
+	}
+	versions := []string{}
+	for _, file := range files {
+		if file != "mercurial" {
+			stat, err := os.Stat(file)
+			if err != nil {
+				logger.Println("while getting installed versions:", err)
+				return nil, err
+			}
+			if stat.IsDir() {
+				if l.isValidVersion(file, tags, sources, binaries) {
+					versions = append(versions, fmt.Sprintf("    %s", file))
+				}
+			}
+		}
+	}
+
+	return versions, nil
+}
+
+// return a list of non installed go versions
+func (l *List) getNonInstalled(v, tags, sources, binaries []string) []string {
+	var versions = make([]string, len(tags)+len(sources)+len(binaries)-len(v))
+	installed_versions := make([]string, len(v))
+	copy(installed_versions, v)
+	c := 0
+	for _, ver := range append(binaries, append(tags, sources...)...) {
+		for i, installed := range installed_versions {
+			if installed == ver {
+				// skip this element and reduce v
+				installed_versions = append(
+					installed_versions[:i], installed_versions[i+1:]...)
+				continue
+			}
+		}
+		versions[c] = fmt.Sprintf("    %s", ver)
+		c++
+	}
+
+	return versions
+}
+
+// check if a given version is valid in all the possible containers
+func (l *List) isValidVersion(file string, tags, sources, binaries []string) bool {
+	// look on the sources first that is the smaller collection
+	for _, ver := range sources {
+		if file == ver {
+			return true
+		}
+	}
+	// now look on the binaries collection using binary search
+	index := sort.SearchStrings(binaries, file)
+	if len(binaries) > index && binaries[index] == file {
+		return true
+	}
+	// now look in the mercurial tags using binary search
+	index = sort.SearchStrings(tags, file)
+	if len(tags) > index && tags[index] == file {
+		return true
+	}
+
+	return false
 }
