4d160624533479e425f2bbadbec0a5bddf9201f0
Oscar Campos <oscar.campos@member.fsf.org>
2014-12-12 22:23:53 +0000

    addapted commands package

--- commands/commands.go
+++ commands/commands.go
@@ -20,13 +20,114 @@
 
 package commands
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+	"text/template"
+
+	"github.com/DamnWidget/VenGO/utils"
+	flag "github.com/ogier/pflag"
+
+	"os"
+)
 
 // type of output format for commands
 const (
 	Text = iota
 	Json
 )
+
+// template constants
+const (
+	// command template
+	commandTpl = `
+Create and manage Isolated Virtual Environments for Golang.
+
+Usage:
+   vengo command [arguments]
+
+Where command can be one of the list below:
+{{ range . }}
+   {{ .Name | Ok | printf "%-8s" }} {{ .Short }}{{ end }}
+
+Use "vengo help command" for detailed information about any command
+`
+	// help template
+	helpTpl = `
+Usage: vengo {{ .Usage }}
+
+{{ .Long }}
+`
+)
+
+var suggest = utils.Ok("suggestion")
+
+// execute command function type
+type commandFunc func(cmd *Command, args ...string)
+
+// command structure
+type Command struct {
+	Name    string       // command name
+	Usage   string       // short line that contains the usage help
+	Short   string       // short description
+	Long    string       // long description
+	Execute commandFunc  // run the command
+	Flag    flag.FlagSet // set of flags for this command
+}
+
+// return a string representation of the command
+func (cmd *Command) String() string {
+	return cmd.Name
+}
+
+// display the usage and exit
+func (cmd *Command) DisplayUsageAndExit() {
+	fmt.Printf("Usage: vengo %s\n\n", cmd.Usage)
+	fmt.Printf("%s: execute 'vengo' with no arguments to get a list of valid commands\n")
+	os.Exit(2)
+}
+
+// used when the commans is not found
+func NonCommand(cmd string) {
+	fmt.Printf("Command '%s' doesn't looks like a valid VenGO command...\n", cmd)
+	fmt.Printf("%s: execute 'vengo' with no arguments to get a list of valid commands\n", suggest)
+}
+
+// help function
+func Help(args ...string) {
+	if len(args) == 0 {
+		usage()
+		return
+	}
+	cmd, ok := Commands[args[0]]
+	if !ok {
+		NonCommand(args[0])
+		os.Exit(2)
+	}
+	t := template.New("help")
+	template.Must(t.Parse(fmt.Sprintf("%s\n\n", strings.TrimSpace(helpTpl))))
+	if err := t.Execute(os.Stdout, cmd); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// calls usage to prints usage information and exits
+func Usage() {
+	usage()
+	os.Exit(2)
+}
+
+// print usage information
+func usage() {
+	t := template.New("usage")
+	t.Funcs(template.FuncMap{"Ok": utils.Ok})
+	template.Must(t.Parse(fmt.Sprintf("%s\n\n", strings.TrimSpace(commandTpl))))
+	if err := t.Execute(os.Stdout, Commands); err != nil {
+		log.Fatal(err)
+	}
+}
 
 // Runner is a interface that wraps the execution of a command
 //
@@ -42,4 +143,9 @@ var ErrNotInstalled = errors.New("Go version not installed")
 // determine if the given error is of ErrNotInstalled type
 func IsNotInstalledError(err error) bool {
 	return err == ErrNotInstalled
+}
+
+// commands map, useful for the help command
+var Commands map[string]*Command = map[string]*Command{
+	cmdList.Name: cmdList,
 }
